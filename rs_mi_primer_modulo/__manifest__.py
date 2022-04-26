9a3458917fb25c5b659678c51d1dd6ef21af1679
Alejandro <adelriocastillo@gmail.com>
2022-04-26 17:17:51 -0500

    [First Version] First Version Module with doddba

---
+++ rs_mi_primer_modulo/__manifest__.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    jeffery CHEN fan<jeffery9@gmail.com>
+#
+#    Copyright (c) All rights reserved:
+#        (c) 2017  TM_FULLNAME
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses
+#
+#    Odoo and OpenERP is trademark of Odoo S.A.
+#
+###############################################################################
+{
+    'name': 'Mi primer odulo Odoo con Doodba',
+    'summary': 'Este es un ejemplo de un modulo',
+    'version': '1.0',
+
+    'description': """
+Modulo par apropbar ambientes de desarrollo
+==============================================
+
+Este es un modulo de prueba
+
+    """,
+
+    'author': 'Alejandro del Rio',
+    'maintainer': 'Alejandro del Rio',
+    'contributors': ['Alejendro del Rio <adelriocastillo@gmail.com>','Edgar Perez <edgarpv@realsystems.com.mx'],
+
+    'website': 'http://www.gitlab.com/TM_FULLNAME',
+
+    'license': 'AGPL-3',
+    'category': 'Uncategorized',
+
+    'depends': [
+        'base'
+    ],
+    'external_dependencies': {
+        'python': [
+        ],
+    },
+    'data': [
+    ],
+    'demo': [
+    ],
+    'js': [
+    ],
+    'css': [
+    ],
+    'qweb': [
+    ],
+    'images': [
+    ],
+    'test': [
+    ],
+
+    'installable': True
+}
